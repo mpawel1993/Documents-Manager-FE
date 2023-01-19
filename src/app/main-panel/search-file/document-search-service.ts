41fa4dea70af8e720cfc4676a8edbf8f35bcf2ea
Pawel Mazur <mpawel1993@gmail.com>
2023-01-19 01:14:59 +0100

    #5 Search page skeleton added

---
+++ src/app/main-panel/search-file/document-search-service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class SearchService{
+
+    //TODO implement calling to API
+    constructor(private http: HttpClient) {
+    }
+
+}
